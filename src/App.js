2aab1d5265ab8b1953a656672d5bf874ea3abfef
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-05-15 19:59:38 +0300

    I am done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [formData, setFormData] = useState({
+        firstName: '',
+        surname: '',
+        phoneNumber: '',
+        emailAddress: '',
+        residence: '',
+        fellowshipName: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/register', formData);
+            alert(response.data.message);
+            setFormData({
+                firstName: '',
+                surname: '',
+                phoneNumber: '',
+                emailAddress: '',
+                residence: '',
+                fellowshipName: ''
+            });
+        } catch (error) {
+            alert('An error occurred. Please try again.');
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Men's Conference Registration</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>First Name:</label>
+                    <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Surname:</label>
+                    <input type="text" name="surname" value={formData.surname} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Phone Number:</label>
+                    <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Email Address:</label>
+                    <input type="email" name="emailAddress" value={formData.emailAddress} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Residence:</label>
+                    <input type="text" name="residence" value={formData.residence} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>House of Prophets Fellowship Name:</label>
+                    <input type="text" name="fellowshipName" value={formData.fellowshipName} onChange={handleChange} required />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
