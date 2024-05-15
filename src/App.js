846c1f962c10ad8afa3a9c7a322eb4d9fd3e6bf7
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-05-15 21:41:43 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,74 +1,78 @@
 // src/App.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
-    const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
+    firstName: '',
+    surname: '',
+    phoneNumber: '',
+    emailAddress: '',
+    residence: '',
+    fellowshipName: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://registrationbackend-amdh.onrender.com/register', formData);
+      toast.success(response.data.message);
+      setFormData({
         firstName: '',
         surname: '',
         phoneNumber: '',
         emailAddress: '',
         residence: '',
         fellowshipName: ''
-    });
+      });
+    } catch (error) {
+      toast.error('An error occurred. Please try again.');
+    }
+  };
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:5000/register', formData);
-            alert(response.data.message);
-            setFormData({
-                firstName: '',
-                surname: '',
-                phoneNumber: '',
-                emailAddress: '',
-                residence: '',
-                fellowshipName: ''
-            });
-        } catch (error) {
-            alert('An error occurred. Please try again.');
-        }
-    };
-
-    return (
-        <div className="App">
-            <h1>Men's Conference Registration</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>First Name:</label>
-                    <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Surname:</label>
-                    <input type="text" name="surname" value={formData.surname} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Phone Number:</label>
-                    <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Email Address:</label>
-                    <input type="email" name="emailAddress" value={formData.emailAddress} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Residence:</label>
-                    <input type="text" name="residence" value={formData.residence} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>House of Prophets Fellowship Name:</label>
-                    <input type="text" name="fellowshipName" value={formData.fellowshipName} onChange={handleChange} required />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+  return (
+    <div className="App">
+      <h1>MAN-UP CONFERENCE 2024</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required />
         </div>
-    );
+        <div>
+          <label htmlFor="surname">Surname:</label>
+          <input type="text" id="surname" name="surname" value={formData.surname} onChange={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input type="text" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="emailAddress">Email Address:</label>
+          <input type="email" id="emailAddress" name="emailAddress" value={formData.emailAddress} onChange={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="residence">Residence:</label>
+          <input type="text" id="residence" name="residence" value={formData.residence} onChange={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="fellowshipName">House of Prophets Fellowship Name:</label>
+          <input type="text" id="fellowshipName" name="fellowshipName" value={formData.fellowshipName} onChange={handleChange} required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
