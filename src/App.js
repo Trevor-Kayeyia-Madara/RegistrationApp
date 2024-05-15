c439edb75f28350b206fd7e18500f5fe222151b1
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-05-15 19:49:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// src/App.js
-import React from 'react';
-import RegistrationForm from './RegistrationForm';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Men's Conference Registration</h1>
-      <RegistrationForm />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
